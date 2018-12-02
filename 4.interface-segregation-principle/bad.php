61e52424923eaa1ad95db95d7ed34dcc8f18ab3d
Hieu Nguyen Duc <isaactesla@gmail.com>
2018-12-02 04:36:15 +0000

    interface segregation principle [runnable]

--- 4.interface-segregation-principle/bad.php
+++ 4.interface-segregation-principle/bad.php
@@ -11,9 +11,115 @@ interface ShapeInterface {
     * Any shape we create must implement the volume method,
     * but we know that squares are flat shapes and that they do not have volumes,
     * so this interface would force the Square class to implement a method that it has no use of.
-    * Any shape we create must implement the volume method,
-    * but we know that squares are flat shapes and that they do not have volumes,
-    * so this interface would force the Square class to implement a method that it has no use of.
     *********************************/
     public function volume();
 }
+
+class Cube implements ShapeInterface {
+    
+    public $length;
+
+    function __construct($length) {
+        $this->length = $length;
+    }
+    
+    public function area() {
+        return 6 * pow($this->length, 2);
+    }
+
+    public function volume() {
+        return pow($this->length, 3);
+    }
+}
+
+/**********************************
+* Any shape we create must implement the volume method,
+* but we know that squares are flat shapes and that they do not have volumes,
+* so this interface would force the Square class to implement a method that it has no use of.
+*********************************/
+class Circle implements ShapeInterface {
+    public $radius;
+
+    function __construct($radius) {
+        $this->radius = $radius;
+    }
+
+    public function area() {
+        return pi() * pow($this->radius, 2);
+    }
+    
+    public function volume() {
+        echo 'do nothing';
+    }
+}
+
+class Square implements ShapeInterface {
+    public $length;
+
+    function __construct($length) {
+        $this->length = $length;
+    }
+
+    public function area() {
+        return pow($this->length, 2);
+    }
+    
+    public function volume() {
+        echo 'do nothing';
+    }
+}
+
+//we have calculators here
+class AreaCalculator {
+    
+    protected $shapes;
+
+    function __construct($shapes = array()) {
+        $this->shapes = $shapes;
+    }
+
+    public function sum() {
+        foreach($this->shapes as $shape) {
+            if(is_a($shape, 'ShapeInterface')) {
+                $area[] = $shape->area();
+                continue;
+            }
+    
+            throw new AreaCalculatorInvalidShapeException;
+        }
+    
+        return array_sum($area);
+    }
+}
+class VolumeCalculator extends AreaCalculator {
+    
+    public function sum() {
+       foreach($this->shapes as $shape) {
+            if(is_a($shape, 'ShapeInterface')) {
+                $area[] = $shape->volume();
+                continue;
+            }
+    
+            throw new VolumeCalculatorInvalidShapeException;
+        }
+    
+        return array_sum($area);
+    }
+}
+
+$shapes = array(
+    new Circle(2),
+    new Square(5),
+    new Square(6)
+);
+
+$solidShapes = array(
+    new Cube(3),
+    new Cube(4),
+);
+
+$areas = new AreaCalculator($shapes);
+$volumes = new VolumeCalculator($solidShapes);
+
+echo $areas->sum() . "\n";
+echo $volumes->sum() . "\n";
--- 4.interface-segregation-principle/good.php
+++ 4.interface-segregation-principle/good.php
@@ -12,12 +12,101 @@ interface SolidShapeInterface {
     public function volume();
 }
 
-class Cuboid implements ShapeInterface, SolidShapeInterface {
+class Circle implements ShapeInterface {
+    public $radius;
+
+    function __construct($radius) {
+        $this->radius = $radius;
+    }
+
     public function area() {
-        // calculate the surface area of the cuboid
+        return pi() * pow($this->radius, 2);
+    }
+}
+
+class Square implements ShapeInterface {
+    public $length;
+
+    function __construct($length) {
+        $this->length = $length;
+    }
+
+    public function area() {
+        return pow($this->length, 2);
+    }
+}
+
+class Cube implements ShapeInterface, SolidShapeInterface {
+    
+    public $length;
+
+    function __construct($length) {
+        $this->length = $length;
+    }
+    
+    public function area() {
+        return 6 * pow($this->length, 2);
     }
 
     public function volume() {
-        // calculate the volume of the cuboid
+        return pow($this->length, 3);
     }
 }
+
+//we have calculators here
+class AreaCalculator {
+    
+    protected $shapes;
+
+    function __construct($shapes = array()) {
+        $this->shapes = $shapes;
+    }
+
+    public function sum() {
+        foreach($this->shapes as $shape) {
+            if(is_a($shape, 'ShapeInterface')) {
+                $area[] = $shape->area();
+                continue;
+            }
+    
+            throw new AreaCalculatorInvalidShapeException;
+        }
+    
+        return array_sum($area);
+    }
+}
+class VolumeCalculator extends AreaCalculator {
+    
+    public function sum() {
+       foreach($this->shapes as $shape) {
+           /**********************************
+            * We use SolidShapeInterface here instead
+            *********************************/
+            if(is_a($shape, 'SolidShapeInterface')) {
+                $area[] = $shape->volume();
+                continue;
+            }
+    
+            throw new VolumeCalculatorInvalidShapeException;
+        }
+    
+        return array_sum($area);
+    }
+}
+
+$shapes = array(
+    new Circle(2),
+    new Square(5),
+    new Square(6)
+);
+
+$solidShapes = array(
+    new Cube(3),
+    new Cube(4),
+);
+
+$areas = new AreaCalculator($shapes);
+$volumes = new VolumeCalculator($solidShapes);
+
+echo $areas->sum() . "\n";
+echo $volumes->sum() . "\n";
